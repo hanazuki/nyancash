d1674d98f76a57b1b912f7c0812ed839b524132f
Kasumi Hanazuki <kasumi@rollingapple.net>
2015-08-26 20:15:20 +0900

    Implement lock

--- lib/nyancash/session.rb
+++ lib/nyancash/session.rb
@@ -2,14 +2,55 @@ module NyanCash
   class Session
     attr_reader :db
 
-    def initialize(db)
+    def initialize(db, hostname, pid)
       @db = db
+      @lock = Lock.new(db, hostname, pid)
 
       Models::All.each do |model|
         model.create(self)
       end
       Models::All.each do |model|
         model.config(self)
+      end
+
+      @lock.acquire
+    end
+
+    def close
+      @lock.release
+    end
+
+    class Lock
+      class AlreadyLocked < Exception
+      end
+
+      def initialize(db, hostname, pid, break_existing: false)
+        @db = db
+        @hostname = hostname.to_s
+        @pid = pid.to_i
+        @break_existing = break_existing
+
+        @locks = db[:gnclock]
+      end
+
+      def acquire
+        @db.transaction(isolation: :serializable) do
+          if (locked = @locks.first)
+            if locked[:Hostname] == @hostname && locked[:PID] == @pid
+              return # this process owns lock
+            end
+
+            unless @break_existing
+              raise AlreadyLocked, "Database is locked by #{locked[:PID]}@#{locked[:Hostname]}"
+            end
+          end
+
+          @locks.insert(Hostname: @hostname, PID: @pid)
+        end
+      end
+
+      def release
+        @locks.where(Hostname: @hostname, PID: @pid).delete
       end
     end
   end
